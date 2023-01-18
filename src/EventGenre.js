0c0fda9cfd88884d0117c0a4f3f3706dd354d1fb
StefaniDurovic <95530637+StefaniDurovic@users.noreply.github.com>
2023-01-18 01:14:56 +0100

    updated EventGenre

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -3,20 +3,21 @@ import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
 const EventGenre = ({ events }) => {
   const colors = ["#524C9A", "#39536C", "#462749", "#ABA9C3", "#635C51"];
-  const getData = () => {
-    const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
-    const data = genres.map((genre) => {
-      const value = events.filter((event) =>
-        event.summary.split(" ").includes(genre)
-      ).length;
-      return { name: genre, value };
-    });
-    return data.filter((entry) => entry.value > 0);
-  };
-
   const [data, setData] = useState([]);
+  
   useEffect(() => {
-    setData(() => getData());
+    const getData = () => {
+      const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS", "Angular"];
+      const data = genres.map((genre) => {
+        const value = events.filter((event) =>
+          event.summary.split(" ").includes(genre)
+        ).length;
+        return { name: genre, value };
+      });
+      return data.filter((entry) => entry.value > 0);
+    };
+  
+    setData(() => getData()); 
   }, [events]);
 
   return (
